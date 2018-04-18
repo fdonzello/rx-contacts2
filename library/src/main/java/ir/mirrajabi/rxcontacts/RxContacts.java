9752174debf8ce285658fbca8387777cbe4fab91
Francesco Donzello <francesco.donzello@gmail.com>
2018-04-18 11:22:50 +0200

    fixes

--- library/src/main/java/ir/mirrajabi/rxcontacts/RxContacts.java
+++ library/src/main/java/ir/mirrajabi/rxcontacts/RxContacts.java
@@ -21,22 +21,20 @@ import android.content.Context;
 import android.database.Cursor;
 import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
+import android.text.TextUtils;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
+import timber.log.Timber;
 
 import static ir.mirrajabi.rxcontacts.ColumnMapper.mapDisplayName;
-import static ir.mirrajabi.rxcontacts.ColumnMapper.mapEmail;
 import static ir.mirrajabi.rxcontacts.ColumnMapper.mapFamilyName;
 import static ir.mirrajabi.rxcontacts.ColumnMapper.mapGivenName;
-import static ir.mirrajabi.rxcontacts.ColumnMapper.mapInVisibleGroup;
-import static ir.mirrajabi.rxcontacts.ColumnMapper.mapPhoneNumber;
-import static ir.mirrajabi.rxcontacts.ColumnMapper.mapPhoto;
-import static ir.mirrajabi.rxcontacts.ColumnMapper.mapStarred;
-import static ir.mirrajabi.rxcontacts.ColumnMapper.mapThumbnail;
 
 
 /**
@@ -48,25 +46,26 @@ import static ir.mirrajabi.rxcontacts.ColumnMapper.mapThumbnail;
 public class RxContacts {
     private static final String[] PROJECTION = {
             ContactsContract.Data.CONTACT_ID,
+            ContactsContract.Data.RAW_CONTACT_ID,
             ContactsContract.Data.DISPLAY_NAME_PRIMARY,
             ContactsContract.CommonDataKinds.StructuredName.GIVEN_NAME,
+            ContactsContract.CommonDataKinds.StructuredName.FAMILY_NAME,
             ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME,
-            ContactsContract.Data.STARRED,
-            ContactsContract.Data.PHOTO_URI,
-            ContactsContract.Data.PHOTO_THUMBNAIL_URI,
-            ContactsContract.Data.DATA1,
-            ContactsContract.Data.MIMETYPE,
-            ContactsContract.Data.IN_VISIBLE_GROUP
+            ContactsContract.CommonDataKinds.Email.ADDRESS,
+            ContactsContract.CommonDataKinds.Email.DATA,
+            ContactsContract.CommonDataKinds.Phone.HAS_PHONE_NUMBER,
+            ContactsContract.CommonDataKinds.Phone.NUMBER
     };
 
     private ContentResolver mResolver;
 
-    public static Observable<Contact> fetch(@NonNull final Context context) {
-        return Observable.create(new ObservableOnSubscribe<Contact>() {
+    public static Observable<List<Contact>> fetch(@NonNull final Context context, final int page) {
+        return Observable.create(new ObservableOnSubscribe<List<Contact>>() {
             @Override
             public void subscribe(@io.reactivex.annotations.NonNull
-                                          ObservableEmitter<Contact> e) throws Exception {
-                new RxContacts(context).fetch(e);
+                                          ObservableEmitter<List<Contact>> e) throws Exception {
+                e.onNext(new RxContacts(context).fetch(page));
+//                e.onComplete();
             }
         });
     }
@@ -76,64 +75,97 @@ public class RxContacts {
     }
 
 
-    private void fetch(ObservableEmitter<Contact> emitter) {
+    private List<Contact> fetch(int page) {
         HashMap<Long, Contact> contacts = new HashMap<>();
-        Cursor cursor = createCursor();
+        Cursor cursor = createCursor(page);
+        Timber.i("Query finished");
         cursor.moveToFirst();
-        int idColumnIndex = cursor.getColumnIndex(ContactsContract.Data.CONTACT_ID);
-        int inVisibleGroupColumnIndex = cursor.getColumnIndex(ContactsContract.Data.IN_VISIBLE_GROUP);
-        int displayNamePrimaryColumnIndex = cursor.getColumnIndex(ContactsContract.Data.DISPLAY_NAME_PRIMARY);
-        int givenNameColumnIndex = cursor.getColumnIndex(ContactsContract.CommonDataKinds.StructuredName.GIVEN_NAME);
-        int familyNameColumnIndex = cursor.getColumnIndex(ContactsContract.CommonDataKinds.StructuredName.FAMILY_NAME);
-        int starredColumnIndex = cursor.getColumnIndex(ContactsContract.Data.STARRED);
-        int photoColumnIndex = cursor.getColumnIndex(ContactsContract.Data.PHOTO_URI);
-        int thumbnailColumnIndex = cursor.getColumnIndex(ContactsContract.Data.PHOTO_THUMBNAIL_URI);
-        int mimetypeColumnIndex = cursor.getColumnIndex(ContactsContract.Data.MIMETYPE);
-        int dataColumnIndex = cursor.getColumnIndex(ContactsContract.Data.DATA1);
+
+        List<Contact> all = new ArrayList<>();
         while (!cursor.isAfterLast()) {
+//            Timber.i("Iterating");
+
+            int idColumnIndex = cursor.getColumnIndex(ContactsContract.Data.RAW_CONTACT_ID);
+            int displayNamePrimaryColumnIndex = cursor.getColumnIndex(ContactsContract.Data.DISPLAY_NAME_PRIMARY);
+            int givenNameColumnIndex = cursor.getColumnIndex(ContactsContract.CommonDataKinds.StructuredName.GIVEN_NAME);
+            int familyNameColumnIndex = cursor.getColumnIndex(ContactsContract.CommonDataKinds.StructuredName.FAMILY_NAME);
+            int hasPhoneNumberColumnIndex = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.HAS_PHONE_NUMBER);
+
             long id = cursor.getLong(idColumnIndex);
-            Contact contact = contacts.get(id);
-            if (contact == null) {
+
+            Contact contact;
+
+            if (contacts.containsKey(id)) {
+                contact = contacts.get(id);
+            } else {
                 contact = new Contact(id);
-                mapInVisibleGroup(cursor, contact, inVisibleGroupColumnIndex);
                 mapGivenName(cursor, contact, givenNameColumnIndex);
                 mapFamilyName(cursor, contact, familyNameColumnIndex);
                 mapDisplayName(cursor, contact, displayNamePrimaryColumnIndex);
-                mapStarred(cursor, contact, starredColumnIndex);
-                mapPhoto(cursor, contact, photoColumnIndex);
-                mapThumbnail(cursor, contact, thumbnailColumnIndex);
                 contacts.put(id, contact);
             }
-            String mimetype = cursor.getString(mimetypeColumnIndex);
-            switch (mimetype) {
-                case ContactsContract.CommonDataKinds.Email.CONTENT_ITEM_TYPE: {
-                    mapEmail(cursor, contact, dataColumnIndex);
-                    break;
-                }
-                case ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE: {
-                    mapPhoneNumber(cursor, contact, dataColumnIndex);
-                    break;
+
+            Cursor ce = mResolver.query(
+                    ContactsContract.CommonDataKinds.Email.CONTENT_URI,
+                    null,
+                    ContactsContract.CommonDataKinds.Email.RAW_CONTACT_ID + " = ?",
+                    new String[]{Long.toString(id)},
+                    null
+            );
+
+            if (ce != null && ce.moveToFirst()) {
+                String email = ce.getString(ce.getColumnIndex(ContactsContract.CommonDataKinds.Email.DATA));
+                contact.getEmails().add(email);
+                ce.close();
+            }
+
+
+            // phone
+            int hasPhone = cursor.getInt(hasPhoneNumberColumnIndex);
+            if (hasPhone > 0) {
+                Cursor cp = mResolver.query(
+                        ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                        null,
+                        ContactsContract.CommonDataKinds.Phone.RAW_CONTACT_ID + " = ?",
+                        new String[]{Long.toString(id)},
+                        null
+                );
+                if (cp != null && cp.moveToFirst()) {
+                    String phone = cp.getString(cp.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                    if (!TextUtils.isEmpty(phone)) {
+                        contact.getPhoneNumbers().add(phone);
+                    }
+                    cp.close();
                 }
             }
+
+
             cursor.moveToNext();
         }
         cursor.close();
+
         for (Long key : contacts.keySet()) {
-            emitter.onNext(contacts.get(key));
+            all.add(contacts.get(key));
         }
-        emitter.onComplete();
+
+        return all;
     }
 
-    private Cursor createCursor() {
+    private Cursor createCursor(int page) {
         String whereName = ContactsContract.Data.MIMETYPE + " = ?";
         String[] whereNameParams = new String[]{ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE};
+
+        String limit = String.format(" LIMIT %d,%d", (page - 1) * 1000, 1000 * page);
+
+
+        Timber.i("Fetching contacts with limit: %s", limit);
 
         return mResolver.query(
                 ContactsContract.Data.CONTENT_URI,
                 PROJECTION,
-                null,
-                null,
-                ContactsContract.Data.CONTACT_ID
+                whereName,
+                whereNameParams,
+                ContactsContract.Data.DISPLAY_NAME_PRIMARY + limit
         );
     }
 }
